fa5c33c25c4dcf2456c955a60da050ef88e509d3
erickloolpez <erickloopeze@hotmail.com>
2025-08-05 21:29:32 -0500

    feat(auth): Adding auth protection to our routes

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex justify-center items-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+  return <div>{children}</div>;
+};
+
+export default MainLayout;
