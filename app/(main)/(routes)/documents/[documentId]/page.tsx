934fdbebc9d503c7e42e186e035a2f8906545762
erickloolpez <erickloopeze@hotmail.com>
2025-08-23 18:18:47 -0500

    feat(cover): Adding the necessary config to upload the cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,67 @@
+"use client";
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import { useParams } from "next/navigation";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Cover } from "@/components/cover";
+
 const DocumentIdPage = () => {
-  return <div></div>;
+  // Usar useParams para obtener los parámetros de la URL
+  const params = useParams();
+  const documentId = params.documentId as Id<"documents">;
+
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    [],
+  );
+
+  const document = useQuery(api.documents.getDocumentById, {
+    id: documentId,
+  });
+
+  const update = useMutation(api.documents.updateDocument);
+
+  const onChange = (content: string) => {
+    update({
+      id: documentId,
+      content,
+    });
+  };
+
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <Cover url={document.coverImage} />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
